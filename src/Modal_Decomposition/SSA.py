6d0e81dc6e10ed5d372ef873a4865f8f50b3fad0
Kolita-Huccika <2215269365@qq.com>
2026-03-29 19:52:49 +0800

    2026.3.29 - Optimize the SSA.decompose function, it's faster now. Please use SSA() which means SSA.decompose_fast().

--- src/Modal_Decomposition/SSA.py
+++ src/Modal_Decomposition/SSA.py
@@ -10,11 +10,12 @@ Lib and Version:  (if None write None)
 Only accessed by:  (must)
     Only __init__.py
 
-Modify:  (must)
-    2026.3.25
-
 Description: (if None write None)
     Realize SSA, and optimize the use of numba and SSAfast(function).
+
+Modify:  (must)
+    2026.3.25 - I forgot.
+    2026.3.29 - Optimize the SSA.decompose function, it's faster now. Please use SSA() which means SSA.decompose_fast().
 """
 
 import numpy as np
@@ -32,9 +33,73 @@ class SSA:
         self.U_ = None
         self.V_ = None
 
-    def decompose(self, S: Union[list, np.ndarray], groups=None) -> np.ndarray:
+
+    def __call__(self, S, groups=None, faster=False):
+        return self.decompose_fast(S, groups, faster)
+
+
+    def hankel(self, S, L):
+        N = len(S)
+        K = N - L + 1
+
+        _S = np.asarray(S)
+        strides = (_S.strides[0], _S.strides[0])
+        return np.lib.stride_tricks.as_strided(_S, shape=(L, K), strides=strides)
+
+    @staticmethod
+    def diagonal_average(X_rec: Union[list, np.ndarray], L: int, K: int) -> np.ndarray:
+        """
+        :param X_rec: 2-dim
+        :param L: int
+        :param K: int
+        :return: rc (2-dim)
+        """
+        if not isinstance(X_rec, np.ndarray):
+            X_rec = np.array(X_rec)
+
+        N = L + K - 1
+        rc = np.zeros(N)
+        for n in range(N):
+            i_min = max(0, n - K + 1)
+            i_max = min(L - 1, n)
+
+            total = 0.0
+            count = 0
+            for i in range(i_min, i_max + 1):
+                j = n - i
+                total += X_rec[i, j]
+                count += 1
+            rc[n] = total / count
+
+        return rc
+
+    @staticmethod
+    def diagonal_average_fast(X_rec: Union[list, np.ndarray], L: int, K:int) -> np.ndarray:
+        """
+        like 'diagonal_average'
+        :param X_rec:
+        :param L:
+        :param K:
+        :return: rc (2-dim)
+        """
+        N = L + K - 1
+        n_idx = np.add.outer(np.arange(L), np.arange(K)).flatten()
+        values = X_rec.flatten()
+
+        Sum = np.bincount(n_idx, weights=values, minlength=N)
+
+        counts = np.bincount(n_idx, minlength=N)
+
+        counts[counts == 0] = 1
+
+        rc = Sum / counts
+
+        return rc
+
+    def decompose(self, S: Union[list, np.ndarray],  groups=None) -> np.ndarray:
         """
         :param S: Signal (1-dim)
+        :param L:
         :param groups: group information, such as: [[0], [1,2], [3,4]] means which components will be merged. If None, return all
 
         :return: IMFs (2-dim)
@@ -43,7 +108,7 @@ class SSA:
             S = np.array(S)
 
         series = np.asarray(S).flatten()
-        N = len(series)
+        N = len(S)
 
         if self.window_size is None:
             L = N // 3
@@ -91,6 +156,56 @@ class SSA:
         self.V_ = VT
 
         return self.components_
+
+    def decompose_fast(self, S: Union[list, np.ndarray], groups=None, faster: bool=False) -> np.ndarray:
+        """
+        :param S: Signal (1-dim)
+        :param L:
+        :param groups: group information, such as: [[0], [1,2], [3,4]] means which components will be merged. If None, return all
+        :param faster: if you choose True, function will be faster with wasting memory.
+        :return: IMFs (2-dim)
+        """
+
+        if not isinstance(S, np.ndarray):
+            S = np.array(S)
+
+        N = len(S)
+
+        if self.window_size is None:
+            L = N // 3
+        else:
+            L = min(self.window_size, N // 2)
+
+        K = N - L + 1
+
+        x = self.hankel(S, L)
+
+        U, s, Vt = np.linalg.svd(x, full_matrices=False)
+
+        if groups is None:
+            groups = [[i] for i in range(len(s))]
+
+        RCs = []
+        for group in groups:
+            X_rec = np.zeros((L, K))
+
+            # X_rec = np.sum(s[group] * np.outer(U[:, group], Vt[group, :]), keepdims=True)
+            def f(i) -> np.ndarray:
+                temp = s[i] * np.outer(U[:, i], Vt[i, :])
+                return temp
+
+            if faster:
+                X_rec = np.add.reduce([f(i) for i in group])  # but, you should consider the memory with this code.
+
+            else:
+                for i in group:
+                    X_rec += f(i)  # also, you can use code above:
+
+            rc = self.diagonal_average_fast(X_rec, L, K)
+
+            RCs.append(rc)
+
+        return np.array(RCs)
 
 
 # def give_fast_SSA() -> Callable:
